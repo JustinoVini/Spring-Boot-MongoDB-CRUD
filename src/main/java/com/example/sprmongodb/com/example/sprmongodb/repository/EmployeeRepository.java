db8733ddb59a4696128272765103efdab175286d
Vinicius Justino <visilva5690@hotmail.com>
2022-06-04 13:13:08 -0300

    Criação dos atributos e Implementação da classe repository

--- src/main/java/com/example/sprmongodb/com/example/sprmongodb/repository/EmployeeRepository.java
+++ src/main/java/com/example/sprmongodb/com/example/sprmongodb/repository/EmployeeRepository.java
@@ -1,4 +1,9 @@
 package com.example.sprmongodb.com.example.sprmongodb.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository {
+    
 }
